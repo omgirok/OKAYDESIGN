6464229e840ebe3f0e08496d91331183dda904be
Orion Ou <ouorion@Orions-MacBook-Air.local>
2017-01-04 17:29:05 -0800

    add init function for onload to give images click popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+function addPop() {
+  var pics = document.getElementsByTagName('img');
+
+  for (var i = 0; i < pics.length; i++) {
+    var image = pics[i];
+    image.setAttribute("onclick", "return popUp(this);");
+    console.log("orion");
+  }
+}
+
+
+function popUp(elmt) {
+  elmt.style.width = "400px";
+  elmt.style.height = "500px";
+}
+
+window.onload = addPop;
